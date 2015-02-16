2418b56b19cc4085641f3d72edce4f853720fa2a
Maxime Lemaire <maxime.lemaire@wattgo.com>
2015-02-16 12:12:21 +0100

    add github repo url

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'mxmxmx:jointjs-all',
   version: '0.9.3',
-  // Brief, one-line summary of the package.
   summary: 'Meteor Package for JointJS 0.9.3 (+ Plugins)',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/wattgo/meteor-jointjs-all',
   documentation: 'README.md'
 });
 
